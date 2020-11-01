4306922e00f486e5a3d559d37097a3e3e94d93a5
dflozano1172 <48407259+dflozano1172@users.noreply.github.com>
2020-11-01 10:12:56 +0000

    Update Sirio_libanes_TS.py

--- MockData/Sirio_libanes_TS.py
+++ MockData/Sirio_libanes_TS.py
@@ -1,49 +1,71 @@
+%reset -f
 import numpy as np
 import pandas as pd
 import time
 import seaborn as sns
 from matplotlib import pyplot as plt
 from sklearn import linear_model
+from sklearn.experimental import enable_iterative_imputer
+from sklearn.impute import IterativeImputer
 import time
 
 t = time.time()
+z_save_list = dir()
 
 path     = r'Kaggle_Sirio_Libanes_ICU_Prediction.xlsx'
-raw_Data = pd.read_excel(path)
-sufix    = ['_MEAN', '_MEDIAN','_DIFF_REL','_DIFF','_MAX','_MIN']
-cols     = raw_Data.columns.tolist()
-raw_Data['AGE_PERCENTIL'] = pd.factorize(raw_Data['AGE_PERCENTIL'])[0]
-
+raw_Data = pd.read_excel(path,na_values = np.NaN)
+raw_Data = raw_Data.replace('', np.NaN)
+#sufix    = ['_MEAN', '_MEDIAN','_DIFF_REL','_DIFF','_MAX','_MIN']
+#cols     = raw_Data.columns.tolist()
+raw_Data['AGE_PERCENTIL'] = pd.factorize(raw_Data['AGE_PERCENTIL'])[0] #transforms string data into numerica data
 
 data     = raw_Data[['PATIENT_VISIT_IDENTIFIER', 'AGE_PERCENTIL', 'GENDER', 'BLOODPRESSURE_DIASTOLIC_MEAN',
                      'BLOODPRESSURE_SISTOLIC_MEAN',	'HEART_RATE_MEAN', 'RESPIRATORY_RATE_MEAN',	'TEMPERATURE_MEAN',	
                      'OXYGEN_SATURATION_MEAN', 'WINDOW', 'ICU']]
 
+no_patients_positive = data[(data['ICU'] == 1) & (data['WINDOW'] == 'ABOVE_12')]['PATIENT_VISIT_IDENTIFIER'].unique().tolist()
+no_patients_negative = data[(data['ICU'] == 0) & (data['WINDOW'] == 'ABOVE_12')]['PATIENT_VISIT_IDENTIFIER'].unique().tolist()
 
 
 patients = data['PATIENT_VISIT_IDENTIFIER'].unique().tolist()
+pat_full_pos = []
+pat_miss_pos = []
+pat_full_neg = []
+pat_miss_neg = []
+for patient in patients:
+    pat_data = data[data['PATIENT_VISIT_IDENTIFIER'] == patient]
+    pat_data = pat_data.assign(Time_Val = pat_data.index - pat_data.iloc[0].name)
+    if not any(pat_data.isnull().any()):       
+        if pat_data['ICU'].sum() == 0:
+            pat_full_neg.append(pat_data.drop(columns=['WINDOW']).values.tolist())
+        else:
+            pat_full_pos.append(pat_data.drop(columns=['WINDOW']).values.tolist())
+    else:
+        if pat_data['ICU'].sum() == 0:
+            pat_miss_neg.append(pat_data.drop(columns=['WINDOW']).values.tolist())
+        else:
+            pat_miss_pos.append(pat_data.drop(columns=['WINDOW']).values.tolist())
+            
+pat_full_pos = np.array(pat_full_pos)
+pat_miss_pos = np.array(pat_miss_pos)
+pat_full_neg = np.array(pat_full_neg)
+pat_miss_neg = np.array(pat_miss_neg)
+
+imp_neg = IterativeImputer(max_iter=10, random_state=0)
+imp_neg.fit(pat_full_neg.mean(axis=0))
+imp_pos = IterativeImputer(max_iter=10, random_state=0)
+imp_pos.fit(pat_full_pos.mean(axis=0))
+
+for i, patient in enumerate(np.rollaxis(pat_miss_pos,0)): pat_full_pos = np.insert(pat_full_pos, len(pat_full_pos),imp_pos.transform(patient), axis = 0)
+for i, patient in enumerate(np.rollaxis(pat_miss_neg,0)): pat_full_neg = np.insert(pat_full_neg, len(pat_full_neg),imp_neg.transform(patient), axis = 0)
+data_imputed = np.concatenate((pat_full_pos ,pat_full_neg),axis=0)
+
+z_save_list.append('data_imputed')
+z_save_list.append('save_list')
+for z_name in dir():
+    if z_name not in z_save_list:
+        del globals()[z_name]
 
 
-data[data['PATIENT_VISIT_IDENTIFIER'] == 1]
-A = data[data['ICU'] == 1]
-pos_patients = A['PATIENT_VISIT_IDENTIFIER'].unique().tolist()
 
-###### Simple Imputer 
-from sklearn.impute import SimpleImputer
-imp_mean = SimpleImputer(missing_values=np.nan, strategy='mean')
-imp_mean.fit([[10, 2, 3], [4, np.nan, 6], [10, 7, 9]])
-SimpleImputer()
-X = [[np.nan, 7, 3], [4, np.nan, 6], [10, np.nan, 9]]
-print(imp_mean.transform(X))
-
-###### Iterative Imputer 
-
-from sklearn.experimental import enable_iterative_imputer
-from sklearn.impute import IterativeImputer
-
-imp = IterativeImputer(max_iter=10, random_state=0)
-imp.fit([[1, 2], [3, 6], [4, 8], [np.nan, 3], [7, np.nan]])
-IterativeImputer(random_state=0)
-X_test = [[np.nan, 2], [6, np.nan], [np.nan, 6]]
-# the model learns that the second feature is double the first
-print(np.round(imp.transform(X_test)))
+print(time.time() - t)
